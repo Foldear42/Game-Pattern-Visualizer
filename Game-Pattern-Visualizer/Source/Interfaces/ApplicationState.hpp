e051cddb1254d6e1ec6ef1c4960acda99afc1542
Foldear42 <ale.V42@protonmail.com>
2025-07-03 16:40:22 +0200

    [FIX] make pure virtual function ApplicationState interface

--- Game-Pattern-Visualizer/Source/Interfaces/ApplicationState.hpp
+++ Game-Pattern-Visualizer/Source/Interfaces/ApplicationState.hpp
@@ -1,7 +1,7 @@
 /**
  * @ Author: Foldear
  * @ Filename: ApplicationState.hpp
- * @ Modified time: 2025-05-26 15:12:21
+ * @ Modified time: 2025-07-03 15:44:32
  * @ Description: Interface for state dependent entities
  */
 
@@ -18,10 +18,10 @@ class Application; // forward declaration : no need to include the file
 class ApplicationState
 {
   public:
-    virtual ~ApplicationState() {}
-    virtual void handleEvent(Application &application, const std::optional<sf::Event> &event) {}
-    virtual void update(Application &application, sf::Time delta) {}
-    virtual void render(sf::RenderWindow &window) {}
+    virtual ~ApplicationState() = default;
+    virtual void handleEvent(Application &application, const std::optional<sf::Event> &event) = 0;
+    virtual void update(Application &application, sf::Time delta) = 0;
+    virtual void render(sf::RenderWindow &window) = 0;
 };
 
 } // namespace GPV
