8997b1b82e2ac949f1ab53e5780e823883307450
Foldear42 <ale.V42@protonmail.com>
2025-11-06 11:29:27 +0100

    [RFCT] delete SceneFlyweightPattern

--- Game-Pattern-Visualizer/Source/Patterns/Flyweight/FlyweightPatternDemo.cpp
+++ Game-Pattern-Visualizer/Source/Patterns/Flyweight/FlyweightPatternDemo.cpp
@@ -2,8 +2,13 @@
 
 namespace GPV
 {
-FlyweightPatternDemo::FlyweightPatternDemo(const Context &context) : ApplicationState(context), m_sceneFlyweightPattern(context.textureManager)
+FlyweightPatternDemo::FlyweightPatternDemo(const Context &context)
+    : ApplicationState(context),
+      m_waterTerrain(context.textureManager.get(TextureID::waterterrain)),
+      m_grassTerrain(context.textureManager.get(TextureID::grassterrain)),
+      m_dirtTerrain(context.textureManager.get(TextureID::dirtterrain))
 {
+    generateMap();
 }
 void FlyweightPatternDemo::handleEvent(Application &application, const std::optional<sf::Event> &event)
 {
@@ -13,6 +18,39 @@ void FlyweightPatternDemo::update(Application &application, sf::Time delta)
 }
 void FlyweightPatternDemo::render(sf::RenderWindow &window)
 {
-    window.draw(m_sceneFlyweightPattern);
+    for (int x = 0; x < ROWS; x++)
+    {
+        for (int y = 0; y < COLS; y++)
+        {
+            window.draw(m_vecExtrinsicStates[x][y]);
+        }
+    }
 }
+
+void FlyweightPatternDemo::generateMap()
+{
+    for (int x = 0; x < ROWS; x++)
+    {
+        m_vecExtrinsicStates.emplace_back(std::vector<TerrainExtrinsic>());
+        for (int y = 0; y < COLS; y++)
+        {
+            if ((rand() % 10) == 0)
+            {
+                m_vecExtrinsicStates[x].emplace_back(m_dirtTerrain);
+            }
+            else
+            {
+                m_vecExtrinsicStates[x].emplace_back(m_grassTerrain);
+            }
+            m_vecExtrinsicStates[x][y].setPosition(sf::Vector2f(x * 16, y * 16));
+        }
+    }
+    int x = rand() % ROWS;
+    for (int y = 0; y < COLS; y++)
+    {
+        // m_vecExtrinsicStates[x][y].setTerrainIntrinsic(m_waterTerrain);
+        // m_vecExtrinsicStates[x][y].setPosition(sf::Vector2f(x * 16, y * 16));
+    }
+}
+
 }  // namespace GPV
--- Game-Pattern-Visualizer/Source/Patterns/Flyweight/FlyweightPatternDemo.hpp
+++ Game-Pattern-Visualizer/Source/Patterns/Flyweight/FlyweightPatternDemo.hpp
@@ -7,13 +7,21 @@
 #include <vector>
 
 #include "Application/Application.hpp"
-#include "SceneFlyweightPattern.hpp"
+#include "Interfaces/Scene.hpp"
+#include "TerrainExtrinsic.hpp"
+#include "TerrainIntrinsic.hpp"
 
 namespace GPV
 {
 class FlyweightPatternDemo : public ApplicationState
 {
-    SceneFlyweightPattern m_sceneFlyweightPattern;
+    static constexpr std::size_t COLS = 100;
+    static constexpr std::size_t ROWS = 100;
+    TerrainIntrinsic m_waterTerrain;
+    TerrainIntrinsic m_grassTerrain;
+    TerrainIntrinsic m_dirtTerrain;
+    std::vector<std::vector<TerrainExtrinsic>> m_vecExtrinsicStates;
+    void generateMap();
 
    public:
     explicit FlyweightPatternDemo(const Context &context);
